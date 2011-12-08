94ca04c9436db809e8732067c9e68860bb9ff385
loukasan@gmail.com <loukasan@gmail.com@50d47b81-c440-bda6-0ad9-9f7f0a75737d>
2011-12-08 00:30:43 +0000

    Added an AckFrame. Not yet tested.

---
+++ trunk/src/java/org/tudelft/aircrack/frame/FCS.java
@@ -0,0 +1,39 @@
+package org.tudelft.aircrack.frame;
+
+import java.nio.ByteBuffer;
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
+
+import nl.flotsam.preon.annotation.BoundNumber;
+
+public class FCS {
+
+	@BoundNumber(size="32")
+	private byte[] crc;
+	
+	public FCS(byte[] crc){
+		if (crc.length!=4) throw new IllegalArgumentException("FCS should be 4 bytes long");
+		
+		for (int i=0;i<crc.length;i++)
+			this.crc[i] = crc[i];
+	}
+	
+	public byte[] getFCS(){
+		return crc;
+	}
+	
+	public static byte[] compute(byte[] data){
+		
+		Checksum checksum = new CRC32();
+        checksum.update(data,0,data.length);
+        
+        return ByteBuffer.allocate(4).putInt((int)checksum.getValue()).array();
+        
+//        return new byte[] {
+//                (byte)(value >>> 24),
+//                (byte)(value >>> 16),
+//                (byte)(value >>> 8),
+//                (byte)value};
+	}
+	
+}
---
+++ trunk/src/java/org/tudelft/aircrack/frame/management/BeaconFrame.java
@@ -0,0 +1,10 @@
+package org.tudelft.aircrack.frame.management;
+
+import org.tudelft.aircrack.frame.Frame;
+
+public class BeaconFrame extends Frame
+{
+	
+	
+
+}
