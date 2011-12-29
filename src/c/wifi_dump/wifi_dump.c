435e1ee2c555312990c20b6ea7e796e6229ef3d1
niels.brouwers <niels.brouwers@gmail.com>
2011-12-29 09:32:00 +0000

    quick&dirty WiFi raw packet dump tool

---
+++ src/c/wifi_dump/wifi_dump.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+#include "osdep.h"
+
+void errx(int code, const char * str)
+{
+}
+
+void err(int code, const char * str)
+{
+}
+
+unsigned long millis()
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
+void dump(FILE * fp, unsigned char * data, int length)
+{
+	fwrite(data, 1, length, fp);
+}
+
+void main()
+{
+	static FILE * traceFile;
+
+	struct wif * wi;
+	struct rx_info rxi;
+	unsigned char buffer[4096];
+	int i, bytesRead;
+	
+	// Open trace file
+	traceFile = fopen("/sdcard/", "wb");
+
+	// Open wireless interface
+	wi = wi_open("mon0");
+	
+	// Channel 11
+	wi_set_channel(wi, 11);
+	printf("CHANNEL: %d\n", wi_get_channel(wi));
+	
+	// Get MAC
+	char mac[6];
+	int ret = wi_get_mac(wi, mac);
+	printf("MAC: %02x:%02x:%02x:%02x:%02x:%02x\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+	// Receive a WiFi packet
+	
+	while (1)
+	{
+	
+		bytesRead = wi_read(wi, buffer, 4096, &rxi);
+
+		// Magic
+		putchar('M');
+		putchar('A');
+		putchar('G');
+		putchar('I');
+		putchar('C');
+
+		// Dump timestamp
+		unsigned long time = millis();
+		dump(traceFile, (unsigned char*)&time, sizeof(unsigned long));
+
+		// Dump RX info
+		dump(traceFile, (unsigned char*)&rxi, sizeof(struct rx_info));
+
+		// Dump current channel
+		int channel = wi_get_channel(wi);
+		dump(traceFile, (unsigned char*)&channel, sizeof(int));
+
+		// Dump payload length
+		dump(traceFile, (unsigned char*)&bytesRead, sizeof(int));
+
+		// Dump packet
+		dump(traceFile, buffer, bytesRead);
+
+	}
+	// never gets here :)
+
+	// Close trace file
+	fclose(traceFile);
+	
+	// Close interface
+	wi_close(wi);
+}
