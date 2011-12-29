21947d7aeafe1be3da17b4a1101c720b59b7495c
niels.brouwers@gmail.com <niels.brouwers@gmail.com@50d47b81-c440-bda6-0ad9-9f7f0a75737d>
2011-12-29 09:22:20 +0000

    Moved aircrack to its own directory off /src/c, added Makefile.android to build the osdep part for Android devices using NDK7

---
+++ trunk/src/c/aircrack-ng/src/version.h
@@ -0,0 +1,9 @@
+#define _MAJ 1
+#define _MIN 1
+#define _SUB_MIN 0
+#define _BETA 0
+#define _RC 0
+#define WEBSITE "http://www.aircrack-ng.org"
+
+extern char * getVersion(char * progname, int maj, int min, int submin, int
+			 svnrev, int beta, int rc);
